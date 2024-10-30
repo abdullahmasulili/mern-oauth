b4f6c02c56e4df697f6403a795a7c5765409f22d
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-30 17:32:12 +0800

    feat(controllers): add auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const admin = require("../config/firebase");
+const User = require("../models/user");
+
+async function authenticateUser(req, res) {
+  const { firebaseToken, firstName, lastName } = req.body;
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(firebaseToken);
+    const { uid, email } = decodedToken;
+
+    const user = await User.findOne({ where: { firebase_uid: uid } });
+
+    if (!user) {
+      const newUser = {
+        firebase_uid: uid,
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        sign_up_timestamp: new Date(),
+      };
+
+      await User.create(newUser);
+    }
+
+    res.status(201).json({
+      message: "Authentication succeed",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({
+      error: "Unauthorized",
+    });
+  }
+}
