d7d47388d1a0b5a1329b3490df432b4c11c79c1f
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-30 11:21:58 +0800

    patch(firebase): use firebase admin sdk

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,8 @@
-require("dotenv").config();
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccount.json");
 
-const firebase = require("firebase/app");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-module.exports = firebase.initializeApp(firebaseConfig);
+module.exports = admin;
