702a0017fc9d294ae217fc923a01ce59409b9c2e
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-10-30 11:02:50 +0800

    feat(database): add database connection check before running the server

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const db = require("./models/index.js");
+const { checkDatabaseConnection } = require("./config/database.js");
 
 app.get("/", (req, res) => {
-  db.sequelize.authenticate().then(() => {
-    res.send("Hello World!");
-    console.log("database connected");
-  });
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+(async () => {
+  await checkDatabaseConnection();
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
