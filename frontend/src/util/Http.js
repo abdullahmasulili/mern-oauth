e097770b8a4f5c47a61032562b002658901763ba
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-02 11:16:33 +0800

    feat(utils/Http): add fetch user/users handler

--- frontend/src/util/Http.js
+++ frontend/src/util/Http.js
@@ -20,4 +20,36 @@ async function handleAuth(payload) {
   }
 }
 
-export { handleAuth };
+async function fetchUsers() {
+  try {
+    const response = await fetch("http://localhost:3000/api/users");
+
+    if (!response.ok) {
+      throw new Error({ message: "Failed to fetch users" });
+    }
+
+    const resData = await response.json();
+
+    return resData;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function fetchUsersByUID(uid) {
+  try {
+    const response = await fetch(`http://localhost:3000/api/users/${uid}`);
+
+    if (!response.ok) {
+      throw new Error({ message: "Failed to fetch user" });
+    }
+
+    const resData = await response.json();
+
+    return resData;
+  } catch (err) {
+    return err;
+  }
+}
+
+export { handleAuth, fetchUsers, fetchUsersByUID };
