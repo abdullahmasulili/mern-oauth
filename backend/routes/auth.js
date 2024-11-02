2c17a68f89ebffdf621d533848a3832d932c91fd
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-02 15:16:14 +0800

    feat(routes): add verify email route for auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   authenticateUser,
   logoutUser,
+  verifyEmail,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/", authenticateUser);
 router.post("/logout", logoutUser);
+router.post("/verify-email", verifyEmail);
 
 module.exports = router;
