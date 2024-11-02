7edf6c230f63401261a4df4822065104358c7dc0
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-02 15:13:09 +0800

    feat(middlewares): add check email verified middleware

---
+++ backend/middlewares/checkEmailMiddleware.js
@@ -0,0 +1,27 @@
+const admin = require("../config/firebase");
+const { Sequelize, sequelize } = require("../models/index");
+
+const User = require("../models/user")(sequelize, Sequelize.DataTypes);
+
+const checkEmailMiddleware = async (req, res, next) => {
+  try {
+    const firebaseUserData = await admin.auth().getUser(req.params.uid);
+    const userData = await User.findOne({
+      where: { firebase_uid: req.params.uid },
+    });
+    const { emailVerified } = firebaseUserData;
+    const { email_verified } = userData;
+
+    if (emailVerified && !email_verified) {
+      await User.update({
+        email_verified: true,
+      });
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = checkEmailMiddleware;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { getUsers, getUserByUID } = require("../controllers/usersController");
+const checkEmailMiddleware = require("../middlewares/checkEmailMiddleware");
 
 router.get("/", getUsers);
-router.get("/:uid", getUserByUID);
+router.get("/:uid", checkEmailMiddleware, getUserByUID);
 
 module.exports = router;
