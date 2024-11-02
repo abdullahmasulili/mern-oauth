10b2d221fede228e740e20a131048171dedefc66
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-11-02 20:06:33 +0800

    patch(Root): force redirect to authentication page

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,5 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function RootLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/auth?mode=login");
+  }, [navigate]);
+
   return <Outlet />;
 }
